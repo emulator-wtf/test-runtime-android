687e120fd821391868f8ee600a90645a6440a866
Madis Pink <1147723+madisp@users.noreply.github.com>
2024-10-15 22:04:05 +0300

    build(deps): bump to latest maven-publish plugin (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("com.android.library") version "8.3.0" apply false
-    id("com.vanniktech.maven.publish") version "0.27.0" apply false
+    id("com.vanniktech.maven.publish") version "0.30.0" apply false
 }

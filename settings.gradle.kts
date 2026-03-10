294f063493b2975883a445095b753cf6dd14d62b
Madis Pink <1147723+madisp@users.noreply.github.com>
2026-03-10 09:46:25 +0200

    chore: update project structure and dependencies (#7)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,21 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+    versionCatalogs {
+        create("fixed") {
+            from(files("gradle/fixed.versions.toml"))
+        }
     }
 }
 
